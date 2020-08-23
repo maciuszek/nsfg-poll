c84698779a91a06195b46aecfaa9024f3554a061
maciuszek <szczecinska.duma@gmail.com>
2020-08-22 22:07:59 -0400

    adjusted configuration for heroku trimmed production services

--- bot.py
+++ bot.py
@@ -3,24 +3,24 @@ from discord.ext import commands
 import logging
 import aiohttp
 
+import os
+
 logging.basicConfig(level=logging.INFO)
 
 extensions = (
     "cogs.poll", 
     "cogs.strawpoll", 
     "cogs.meta", 
-    "cogs.owner",
-    "cogs.BackgroundTasks"
+    "cogs.owner"
     )
 
 class PollBot(commands.AutoShardedBot):
-    def __init__(self, config):
+    def __init__(self):
         prefixes = ["+", "poll:", "Poll:", "POLL:"]
         super().__init__(
             command_prefix = prefixes,
             status = discord.Status.online,
             activity = discord.Game(name = "+help"))
-        self.config = config
 
         self.remove_command("help")
         
@@ -39,4 +39,4 @@ class PollBot(commands.AutoShardedBot):
             await self.process_commands(message)
 
     def run(self):
-        super().run(self.config["discord_token"], reconnect=True)
+        super().run(os.getenv('BOT_TOKEN'), reconnect=True)
--- launcher.py
+++ launcher.py
@@ -3,10 +3,6 @@
 import discord
 from bot import PollBot
 import sys
-import json
 
-with open('config.json') as config_file:
-    config = json.load(config_file)
-
-bot = PollBot(config)
+bot = PollBot()
 bot.run()
